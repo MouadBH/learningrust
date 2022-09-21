1eac1413e269d126baf96cfef49ff7d0bd01722d
MouadBH <mouad123b@gmail.com>
2022-09-21 18:08:37 +0100

    hello word using cargo

--- 2.hello_cargo/target/debug/deps/hello_cargo-3c7f7937e34688ab.d
+++ 2.hello_cargo/target/debug/deps/hello_cargo-3c7f7937e34688ab.d
@@ -1,5 +1,5 @@
-/Users/mo3ad/codes/learningrust/2.hello_cargo/target/debug/deps/hello_cargo-3c7f7937e34688ab: src/main.rs
+/Users/mo3ad/codes/learningrust/hello_cargo/target/debug/deps/hello_cargo-3c7f7937e34688ab: src/main.rs
 
-/Users/mo3ad/codes/learningrust/2.hello_cargo/target/debug/deps/hello_cargo-3c7f7937e34688ab.d: src/main.rs
+/Users/mo3ad/codes/learningrust/hello_cargo/target/debug/deps/hello_cargo-3c7f7937e34688ab.d: src/main.rs
 
 src/main.rs:
