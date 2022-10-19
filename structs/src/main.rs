b2566d4e3ca468f04fb5a4ed6d5e3b82857ea83b
MouadBH <mouad123b@gmail.com>
2022-10-19 22:52:41 +0100

    wip structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,40 @@
-fn main() {  
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn main() {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    // create instance from other instance
+    let user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("another@example.com"),
+        sign_in_count: user1.sign_in_count,
+    };
+    
+    // or 
+    let user3 = User {
+        email: String::from("another@example.com"),
+        ..user2
+    };
+
     println!("Hello, world!");
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
