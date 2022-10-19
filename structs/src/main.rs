1d9030d136e8e2da92fa1087cf76312166f890fd
MouadBH <mouad123b@gmail.com>
2022-10-19 22:55:13 +0100

    wip structs tuple structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,6 +5,9 @@ struct User {
     sign_in_count: u64,
 }
 
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
 fn main() {
     let user1 = User {
         email: String::from("someone@example.com"),
@@ -26,7 +29,10 @@ fn main() {
         email: String::from("another@example.com"),
         ..user2
     };
-
+    
+    let black = Color(0, 0 , 0);
+    let origin = Point (0,0,0);
+    
     println!("Hello, world!");
 }
 
