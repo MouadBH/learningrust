0ad602b93762252c74bc1c36955f5fd25f92effc
MouadBH <mouad123b@gmail.com>
2022-10-25 23:01:45 +0100

    wip enums

---
+++ enumm/src/main.rs
@@ -0,0 +1,23 @@
+enum IpAddrKind {
+    V4,
+    V6
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    println!("Hello, world!");
+}
+
+fn route(ip_kind: IpAddrKind) {}
